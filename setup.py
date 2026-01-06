331c08d2eed1a6c2d9e9ed20af32b351ef028faa
mbwmm6 <mbwmm6@gmail.com>
2026-01-06 20:14:31 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 from setuptools import setup
 
 setup(
     name="pygit",
     version="0.1",
-    package=["pygit"],
+    packages=["pygit"],
     entry_points={"console_scripts": ["pygit = pygit.cli:main"]},
 )
