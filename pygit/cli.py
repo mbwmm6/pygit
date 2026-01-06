4ffa1c891b236784ade35b915314f6aac9e099dd
mbwmm6 <mbwmm6@gmail.com>
2026-01-06 20:52:31 +0300

    add function "write-tree"

--- pygit/cli.py
+++ pygit/cli.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 
+from . import base
 from . import data
 
 
@@ -25,6 +26,9 @@ def parse_args():
     cat_file_parse = commands.add_parser("cat-file")
     cat_file_parse.set_defaults(func=cat_file)
 
+    write_tree_parse = commands.add_parser("write-tree")
+    write_tree_parse.set_defaults(func=write_tree)
+
     return parser.parse_args()
 
 
@@ -40,4 +44,8 @@ def hash_object(args):
 
 def cat_file(args):
     sys.stdout.flush()
-    sys.stdout.buffer.write()
+    sys.stdout.buffer.write(data.get_object(args.object, expected=None))
+
+
+def write_tree(args):
+    print(base.write_tree())
